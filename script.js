2999cc2439ead76feefd49ea788f19915e7146e9
lomerci <bunie.emegwa@gmail.com>
2024-11-02 18:32:37 -0500

    Fix the grid-size code so the pixel value would not be changed when the number of squares per side increased

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const gridContainer = document.getElementById('grid-container');
 const colorPicker = document.getElementById('colorPicker');
+const newGrid = document.getElementById('grid-size');
 
-for (let i = 0; i < 16 * 16; i++) {
-        const gridItem = document.createElement('div');
+function createGrid(squaresPerSide) {
+    gridContainer.innerHTML = '';
+    
+    const totalSquares = squaresPerSide * squaresPerSide;
+    const squareSize = 400 / squaresPerSide; 
+
+    for (let i = 0; i < totalSquares; i++) {
+        let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
+        gridItem.style.width = `${squareSize}px`;
+        gridItem.style.height = `${squareSize}px`;
         gridContainer.appendChild(gridItem);
     }
+}
 
-    gridContainer.addEventListener('click', (e) => {
-        if (e.target.classList.contains('grid-item')) {
-            e.target.style.backgroundColor = colorPicker.value;
-        }
-    });  
+createGrid(16);
+
+newGrid.addEventListener('click', () => {
+    let userInput = prompt('How many squares per side do you want? Max: 100');
+    userInput = parseInt(userInput);
+
+    if (isNaN(userInput) || userInput > 100 || userInput < 1) {
+        alert('It cannot be greater than 100 and must be a valid number!');
+        return;
+    }
+    createGrid(userInput);
+});
+
+
+gridContainer.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+
+document.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+gridContainer.addEventListener('mousemove', (e) => {
+    if (isDrawing && e.target.classList.contains('grid-item')) {
+        e.target.style.backgroundColor = colorPicker.value;
+    }
+});
